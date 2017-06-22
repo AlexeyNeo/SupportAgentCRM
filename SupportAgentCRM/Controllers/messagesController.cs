42533e5fba29cfc8a1dc6cbb146a9978855a30c0
AlexeyNeo <htsones63@gmail.com>
2017-06-22 11:14:36 +0600

    Добавлене messames контроллер.

---
+++ SupportAgentCRM/Controllers/messagesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SupportAgentCRM.Controllers
+{
+    public class messagesController : ApiController
+    {
+        // GET: api/messages
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/messages/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/messages
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/messages/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/messages/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
