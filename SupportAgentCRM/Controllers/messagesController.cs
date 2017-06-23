8259e34e3720ff06325702d23c750d484ccfe126
AlexeyNeo <htsones63@gmail.com>
2017-06-23 09:07:01 +0600

    сест

--- SupportAgentCRM/Controllers/messagesController.cs
+++ SupportAgentCRM/Controllers/messagesController.cs
@@ -5,18 +5,18 @@ namespace SupportAgentCRM.Controllers
 {
     public class Messages
     {
-        public IEnumerable<GMailAPILibrary.Message> gmail { get; set; }
-        public IEnumerable <ChatHelpdescAgent.Client> chat2desk { get; set; }
+        public IEnumerable <GMailAPILibrary.Message> gmail { get; set; }
+        public IEnumerable <ChatHelpdescAgent.Message> chat2desk { get; set; }
     }
     public class messagesController : ApiController
     {
         // GET: api/messages
         public Messages Get()
         {
-            ChatHelpdescAgent.Clients clients = new ChatHelpdescAgent.Clients();
-            Messages msg = new Messages();
+            var clients = new ChatHelpdescAgent.Clients();
+            var msg = new Messages();
             msg.gmail = GMailAPILibrary.Message.GetMessages();
-            msg.chat2desk = clients.GetList();
+            msg.chat2desk = ChatHelpdescAgent.Messages.GetMessages("From_client", true,10, false).messages;
             return msg;
         }
 
