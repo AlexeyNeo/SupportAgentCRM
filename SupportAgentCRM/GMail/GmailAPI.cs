a39e33eb97536396228e6fe53b17be47d4aeee3d
FulcrumVlsM <misha867458@gmail.com>
2017-07-10 14:04:36 +0600

    Изменен адрес создания файла TOKERRESPONSE-USER

--- SupportAgentCRM/GMail/GmailAPI.cs
+++ SupportAgentCRM/GMail/GmailAPI.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading;
 using GMailAPILibrary;
 using Message = Google.Apis.Gmail.v1.Data.Message;
+using Google.Apis.Util.Store;
 
 public static class GmailApi
 {
@@ -22,7 +23,7 @@ public static class GmailApi
     {
         var clientSecretData = Encoding.ASCII.GetBytes(ApiHelper.ClientSecret);
         var stream = new MemoryStream(clientSecretData);
-        var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None).Result;
+        var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(GoogleClientSecrets.Load(stream).Secrets, Scopes, "user", CancellationToken.None, new FileDataStore(@"E:\service", true)).Result;
 
         var service = new GmailService(new BaseClientService.Initializer()
         {
